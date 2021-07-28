c150fc465cac22ddbd690e8308cebf307c424c4d
Long Hồ <thienlong460@gmail.com>
2021-07-28 23:55:01 +0700

    i dont know

--- server/server/routes/protected/teacher/subject.js
+++ server/server/routes/protected/teacher/subject.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../../../connection');
 
-router.get('/:semester_id', (req, res)=>{
+router.get('/listsubject', (req, res)=>{
     if(!req.privilege.listsubject_GV) return res.sendStatus(401);
-
-    var semester_id = req.params.semester_id;
-    var user_id = req.body.user_id;
+    console.log("listSubject (GV)");
+    var semester_id = req.query.semester_id;
+    var user_id = req.query.user_id;
 
     connection.query(
         'call listSubject_GV(?,?)',
-        [semester_id,user_id],
+        [user_id, semester_id],
         (err, results, fields)=>{
             if(err) return res.status(500).send(err);
             res.send(results[0]);
         }
     )
 });
+
 
 
 
